da7f354869bbaa63ac808a2423884924a929f2b8
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-01-14 00:09:25 +0000

    Add basic gemspec

---
+++ lib/cccd/cli.rb
@@ -0,0 +1,8 @@
+require "cccd/cli/version"
+
+module Cccd
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/cccd/cli/version.rb
@@ -0,0 +1,5 @@
+module Cccd
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
